a8a8882475c0a4ff42215801a9d6f9ee244a2e9d
Jared Whalen <jared.m.whalen@gmail.com>
2025-01-17 19:47:18 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+  import Window from "./components/Window.svelte";
+  import DwEmbed from "./components/DWEmbed.svelte";
+  // Handle responsive iframes for embeds
+  import pym from "pym.js";
+
+  new pym.Child({
+    polling: 500,
+  });
+
+  let width;
+
+  let unmitigated = 34111;
+  let mitigated = 9666;
+  let unit = 100;
+</script>
+
+<Window />
+<!-- Outer div must have class 'chart-container' don't change -->
+<div class="chart-container">
+  <h1 class="headline">Decline in NFIP policies-in-force in Louisiana</h1>
+  <p class="dek">
+    Rising premiums under FEMA’s Risk Rating 2.0, implemented in 2022, have
+    driven a significant drop in National Flood Insurance Program (NFIP)
+    policies in Louisiana. As affordability becomes a growing concern, fewer
+    residents are able to maintain coverage, leaving communities more vulnerable
+    to flooding and increasing financial strain for those who remain insured.
+  </p>
+
+  <p class="sr-only">
+    Bar chart showing the number of active NFIP policies in Louisiana from 2020
+    to 2024, with a steady decline starting in 2022. The chart highlights the
+    impact of rising premiums under Risk Rating 2.0 on policy retention.
+  </p>
+
+  <div id="g-viz" bind:clientWidth={width}>
+    <DwEmbed />
+  </div>
+
+  <div class="credit">
+    Data: <a
+      target="_blank"
+      href="https://www.fema.gov/openfema-data-page/nfip-multiple-loss-properties-v1"
+      >FEMA</a
+    >; Graphic by Jared Whalen /
+    <a target="_blank" href="https://agwaterdesk.org/">Ag & Water Desk</a>
+  </div>
+</div>
+
+<style lang="scss">
+  .chart-container {
+    max-width: 800px;
+    width: 100%;
+    padding: 2px;
+  }
+</style>
---
+++ src/components/DWEmbed.svelte
@@ -0,0 +1,14 @@
+<div style="min-height:356px" id="datawrapper-vis-KzFxw">
+  <script
+    type="text/javascript"
+    defer
+    src="https://datawrapper.dwcdn.net/KzFxw/embed.js"
+    charset="utf-8"
+    data-target="#datawrapper-vis-KzFxw"
+  ></script><noscript
+    ><img src="https://datawrapper.dwcdn.net/KzFxw/full.png" alt="" /></noscript
+  >
+</div>
+
+<style lang="scss">
+</style>
